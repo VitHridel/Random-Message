eb3748e1804cdbe89a6cc025826f35c96c2b97f3
Vitek Hridel <vitek15hridel@gmail.com>
2021-08-28 23:51:46 +0200

    create arrays for random messages

---
+++ script.js
@@ -0,0 +1,11 @@
+let routines = ['Code', 'Work Out', 'Learn to school', 'Just rest'];
+let music = [
+    'Yzomandias'['Melanž', 'DDSZZ', 'Mikasa Sukasa', 'Zhora Vypadá Všechno Líp', 'Pod Vlivem EP'], 
+    'Separ'['Pirát', 'Pancier'],
+    'DMS'['MDMS', 'Prepáčte'], 
+    'NikTendo'['Sedm', 'Jsem v pohodě, sem v prdeli', 'Fatamorgana', 'Restart', 'Lunazar'], 
+    'Lil Peep'['Star Shopping', 'HELLBOY', 'When You\' re Sober, pt.1', 'When You\' re Sober, pt.2', 'beamer boy', 'white wine', 'white tee'], 
+    'Lil Uzi Vert'['Drankin N Smokin', 'Luv Is Rage 2', 'Eternal Atake', 'Money Longer']
+];
+let shows = ['Interstellar', 'Money Heist', 'Kill Bill', 'Coach Carter', 'Avengers', 'Deathnote', 'The Big Bang Theory', 'twitch.tv/'['Agraelus', 'Claina', 'RobDiesALot', 'CzechCloud'], 'nothing'];
+let games = ['Sekiro: Shadows Die Twice', 'Nioh 2', 'Red Dead Redemption II', 'Chess', 'nothing'];
